4c7949c064adc1f8fe109bf9d38496dbbd8184c4
TheCrunchy <crunchycamsta@gmail.com>
2024-07-30 19:27:11 +0100

    filterable grid

--- Components/Stores/LiveStoreDataComponent.razor.cs
+++ Components/Stores/LiveStoreDataComponent.razor.cs
@@ -19,8 +19,26 @@ namespace FluentUI.Components.Stores
 
         [Inject] private LiveStoreDataService _storeLogsService { get; set; }
         PaginationState pagination = new PaginationState { ItemsPerPage = 25 };
-        private IQueryable<KeenNPCStoreEntry> GridData { get; set; }
+        private IQueryable<KeenNPCStoreEntry> UnfilteredData { get; set; }
+        private IQueryable<KeenNPCStoreEntry> GridData
+        {
+            get
+            {
+                var result = UnfilteredData;
+                if (result is not null && !string.IsNullOrEmpty(typeFilter))
+                {
+                    result = result.Where(c => c.TypeId.Contains(typeFilter, StringComparison.CurrentCultureIgnoreCase));
+                }
+                if (result is not null && !string.IsNullOrEmpty(subTypeFilter))
+                {
+                     result = result.Where(c => c.SubTypeId.Contains(subTypeFilter, StringComparison.CurrentCultureIgnoreCase));
+                }
 
+                return result;
+            }
+        }
+        private string typeFilter;
+        private string subTypeFilter;
         protected override async Task OnInitializedAsync()
         {
             _storeLogsService.DataRefreshed += async (Guid id) => { await LoadData(id); };
@@ -31,11 +49,40 @@ namespace FluentUI.Components.Stores
             if (serverId == ServerId)
             {
                 var data = _storeLogsService.GetKeenStoreEntries(ServerId);
-                GridData = data.AsQueryable();
+                UnfilteredData = data.AsQueryable();
                 await InvokeAsync(StateHasChanged);
-
             }
+        }
 
+        private void HandleTypeFilter(ChangeEventArgs args)
+        {
+            if (args.Value is string value)
+            {
+                typeFilter = value;
+            }
+        }
+        private void HandleSubTypeFilter(ChangeEventArgs args)
+        {
+            if (args.Value is string value)
+            {
+                subTypeFilter = value;
+            }
+        }
+
+        private void HandleClear()
+        {
+            if (!string.IsNullOrWhiteSpace(typeFilter))
+            {
+                typeFilter = string.Empty;
+            }
+        }
+
+        private void HandleSubtypeClear()
+        {
+            if (!string.IsNullOrWhiteSpace(subTypeFilter))
+            {
+                subTypeFilter = string.Empty;
+            }
         }
     }
 }
