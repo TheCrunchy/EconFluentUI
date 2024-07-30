ede56aa0bde375cbc71a60f7e927347b91367e6e
TheCrunchy <crunchycamsta@gmail.com>
2024-07-30 19:18:59 +0100

    it works, need to refine what data i send and show

--- Components/Stores/LiveStoreDataComponent.razor.cs
+++ Components/Stores/LiveStoreDataComponent.razor.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Econ.Interfaces;
+using Econ.Models.Stores;
+using Econ.Services;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.FluentUI.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +14,28 @@ namespace FluentUI.Components.Stores
 {
     public partial class LiveStoreDataComponent
     {
+        [Parameter] public Guid ServerId { get; set; }
 
+
+        [Inject] private LiveStoreDataService _storeLogsService { get; set; }
+        PaginationState pagination = new PaginationState { ItemsPerPage = 25 };
+        private IQueryable<KeenNPCStoreEntry> GridData { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            _storeLogsService.DataRefreshed += async (Guid id) => { await LoadData(id); };
+        }
+
+        public async Task LoadData(Guid serverId)
+        {
+            if (serverId == ServerId)
+            {
+                var data = _storeLogsService.GetKeenStoreEntries(ServerId);
+                GridData = data.AsQueryable();
+                await InvokeAsync(StateHasChanged);
+
+            }
+
+        }
     }
 }
